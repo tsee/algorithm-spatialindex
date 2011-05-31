5a0b3fa4fab222f5bf16a6611a26ca71cdc91736
Steffen Mueller <smueller@cpan.org>
2011-06-01 00:29:26 +0200

    Copyright update

--- lib/Algorithm/SpatialIndex/Strategy.pm
+++ lib/Algorithm/SpatialIndex/Strategy.pm
@@ -220,7 +220,7 @@ Steffen Mueller, E<lt>smueller@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2010 by Steffen Mueller
+Copyright (C) 2010, 2011 by Steffen Mueller
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.10.1 or,
